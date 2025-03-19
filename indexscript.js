7368c741bb7eb7d1883b0b1a59ac880e3182d459
saabdo42 <sayraa.abdo11@gmail.com>
2025-03-19 16:00:55 -0500

    optimized js
    yippeeeee

--- indexscript.js
+++ indexscript.js
@@ -1,9 +1,9 @@
-var navlinks = document.querySelectorAll(".navlink");
+var navlinks = document.getElementsByClassName("navlink");
 var navlinklist = Array.from(navlinks);
 var logo = document.getElementById("logo");
 var copyright = document.getElementById("copyright");
 var logosrcs = ["images/logogreen.png", "images/logoblue.png", "images/logopink.png"];
-var colors = [ "#96cc72", "#b4d8f2", "#f6b1cf" ]
+var colors = [ "#96cc72", "#b4d8f2", "#f6b1cf" ];
 var logoversion = 0;
 var logochanged = false;
 
@@ -17,140 +17,141 @@ var dimmer2 = document.getElementById("dimmer2");
 
 var sections = document.querySelectorAll("section");
 var sectionlist = Array.from(sections);
-
-var thms = document.querySelectorAll(".artthmtitle");
+var thms = document.getElementsByClassName("artthmtitle");
 var thmlist = Array.from(thms);
+var projects = document.getElementsByClassName("drop");
+var projectlist = Array.from(projects);
 
-var drops = document.querySelectorAll(".drop");
-var droplist = Array.from(drops);
+var screenheight;
+var numberofsections;
+var ismobile;
 
 document.addEventListener("keydown", keyboardShortcuts);
 
-let timeoutover = true; //timeout for the scroll so ur computer doesn't crash
+timeoutover = true; //timeout for the scroll so ur computer doesn't crash
 function retimer(){
     timeoutover = true;
 }
- 
+
+function settingvariables(){
+    screenheight = window.innerHeight;
+    numberofsections = sectionlist.length;
+    ismobile = (window.innerWidth < 600 );
+}
+
 function keyboardShortcuts(event) { //keyboard shortcuts
 
+    event.preventDefault();
+
     if (event.key === "ArrowLeft") {
-        event.preventDefault();
         bac();
     }else if (event.key === "ArrowRight"){
-        event.preventDefault();
         nex();
     }else if (event.key === "Escape" ){
         closedrop();
-    }else{
-        return;
     }
 }
 
 function logoswitcher(){ //switches the logo when you scroll past it :)
-    
-    logo.src = logosrcs[logoversion];
-    copyright.style.color = colors[logoversion];
 
-    if (scrollY > window.innerHeight && logochanged == false ){
-        logochanged = true;
-        logoversion++;
-    }
-    if (scrollY < window.innerHeight ){
-        logochanged = false;
-    }
-    if (logoversion > logosrcs.length - 1 ){
-        logoversion = 0;
-    }
+    if ( timeoutover = false ){
+        return;
+    }else if ( scrollY > screenheight * 2 ){
+        return;
+    }else{
+        logo.src = logosrcs[logoversion];
+        copyright.style.color = colors[logoversion];
+
+        if ( scrollY > screenheight && logochanged == false ){
+            logochanged = true;
+            logoversion++;
+        }
+        if ( scrollY < screenheight ){
+            logochanged = false;
+        }
+        if ( logoversion > logosrcs.length - 1 ){
+            logoversion = 0;
+        }
+    }    
 }
 
-function isvisible(check){ //checks for visible
-    if (check.classList.contains("vis")){
+function isvisible(object){ //checks for visible
+    if ( object.classList.contains("vis") ){
         return true;
-    }else if (check.classList.contains("invis")){
+    }else if ( object.classList.contains("invis") ){
         return false;
     }
 }
 
-function hide(object){ //hides
-    object.classList.replace("vis", "invis");
-
-}
-
-function reveal(object){ //reveals
-    object.classList.replace("invis", "vis");
-}
-
-function mobilehidenav() {
-    if (window.innerWidth > 600){
-        nav.classList = "vis"
-    }else{
-        nav.classList = "invis"
-        burg.classList = "vis"
+function hide(object){ //hides an object
+    if ( object ){
+        object.classList.replace("vis", "invis"); //got errors for trying to hide a pdf when it didn't exist
     }
 }
 
-function burger() {
-    if (isvisible(nav) == true){
-        hide(nav)
-    }else{
-        reveal(nav)
-    } 
+function show(object){ //shows an object
+    if ( object ){
+        object.classList.replace("invis", "vis");
+    }
+}
+
+function mobilehidenav() { //hides menu on mobile
+    switch( ismobile ){
+        case false:
+            show(nav);
+            break;
+        case true:
+            hide(nav);
+            show(burg);
+    }
+}
+
+function burger() { //opens hamburger menu
+    switch( isvisible(nav) ){
+        case true:
+            hide(nav);
+            break;
+        case false:
+            show(nav);
+    }
 }
 
 function mobilenavclick() { //hides the nav after clicking a link on mobile
-    if ( window.innerWidth < 600 ){
+    if ( ismobile ){
         hide(nav);
     }
 }
 
-function highlightsection() { //highlights the nav link of what section ur scrolled to 
+function highlightsection(){ //highlights navlink of current section
 
-    if (timeoutover == false){
+    var currentnavlink = 0;
+    
+    if ( timeoutover == false ){
         return;
     }else{
-
         timeoutover = false;
-        setTimeout(retimer, 100);
-
-        if (scrollY < sectionlist[0].offsetTop ){ 
-            
-            for (i=0; i < navlinklist.length; i++){
-                navlinklist[i].classList.replace("current", "notcurrent");
+        setTimeout(retimer, 300);
+        
+        for ( i=0; i < numberofsections; i++ ){ //for each section
+            if ( sectionlist[i].getBoundingClientRect().top < (screenheight/2) && sectionlist[i].getBoundingClientRect().bottom > (screenheight/2)){ //if it's on screen
+                navlinklist[i].classList.replace("notcurrent", "current"); //make this navlink current
+                currentnavlink = i;
             }
-
-        } else if( scrollY > sectionlist[0].offsetTop && scrollY < sectionlist[1].offsetTop ){
-
-            for (i=0; i < navlinklist.length; i++){
-                navlinklist[i].classList.replace("current", "notcurrent");
-            }
-            navlinklist[0].classList.replace("notcurrent", "current");
-
-        } else if( scrollY > sectionlist[1].offsetTop && scrollY < sectionlist[2].offsetTop ){
-
-            for (i=0; i < navlinklist.length; i++){
-                navlinklist[i].classList.replace("current", "notcurrent");
-            }
-            navlinklist[1].classList.replace("notcurrent", "current");
-
-        } else if( scrollY > sectionlist[2].offsetTop && scrollY < sectionlist[3].offsetTop ){
-
-            for (i=0; i < navlinklist.length; i++){
-                navlinklist[i].classList.replace("current", "notcurrent");
-            }
-            navlinklist[2].classList.replace("notcurrent", "current");
-
-        } else if ( scrollY > sectionlist[3].offsetTop ){
-            for (i=0; i < navlinklist.length; i++){
-                navlinklist[i].classList.replace("current", "notcurrent");
-            }
-            navlinklist[3].classList.replace("notcurrent", "current");
+        }
+        for ( i=0; i < numberofsections; i++ ){ //for each section 
+            if ( i != currentnavlink ){ //except the current
+                navlinklist[i].classList.replace("current", "notcurrent"); //make it notcurrent
+            }            
         }
     }
 }
 
-function closedrop() {  //loop hide all descriptions
-    for (i = 0; i < droplist.length; i++) {
-        hide(droplist[i]);
+function closedrop(){ //closes open project
+
+    for ( i=0; i<projectlist.length; i++ ){ //check which project is visible & hide it
+        if ( isvisible(projectlist[i]) ){
+            hide(projectlist[i]);
+        }
     }
     hide(backbutton);
     hide(nextbutton);
@@ -158,110 +159,87 @@ function closedrop() {  //loop hide all descriptions
     hide(dimmer2);
 }
 
-function opendesc(clickedthm) {  //reveal description of the box u clicked on
+function openproject(clickedthm) {  //open clicked thumbnail
 
-    var whichthm = thmlist.indexOf(clickedthm);
+    var whichproject = thmlist.indexOf(clickedthm);
 
-    var bigimgs = droplist[whichthm].getElementsByClassName("bigimg");  //expanding the first thmnail
-    var thmnail = droplist[whichthm].getElementsByClassName("descimg");
-    bigimgs[0].src = thmnail[0].src;
-
-    if ( !bigimgs[0].parentElement.parentElement.parentElement.classList.contains("haspdf")){
-        reveal(bigimgs[0]);
-    }
-
-    reveal(droplist[whichthm]);
-    reveal(backbutton);
-    reveal(nextbutton);
-    reveal(closebutton);
-    reveal(dimmer2);
+    show(projectlist[whichproject]);
+    displayfirstdetail(projectlist[whichproject]);
+    show(backbutton);
+    show(nextbutton);
+    show(closebutton);
+    show(dimmer2);
 }
 
-function bac(){
-    var currentvisdesc = undefined
+function bac(){ //goes back one project
 
-    for (j = 0; j <droplist.length; j++) { //checking which desc isvisible
-        
-        if ( isvisible(droplist[j]) == true) {
-            currentvisdesc = j; //storing it
+    var currentvisproj
+
+    for ( i = 0; i <projectlist.length; i++ ) { //checking which project isvisible 
+        if ( isvisible(projectlist[i]) ) {
+            currentvisproj = i;
         }
     }
 
-    nextvisdesc = currentvisdesc - 1; //setting previous desc
-    if (nextvisdesc < 0){ //on the first desc, previous desc is the last desc.....
-        nextvisdesc = droplist.length -1; 
+    prevproj = currentvisproj - 1; //finding previous project
+    if ( prevproj < 0 ){ //looping array or something
+        prevproj = projectlist.length - 1;
     }
 
-    hide(droplist[currentvisdesc]);
-    reveal(droplist[nextvisdesc]);
-
-    var bigimgs = droplist[nextvisdesc].getElementsByClassName("bigimg");
-    var thmnails = droplist[nextvisdesc].getElementsByClassName("descimg");
-
-    if (droplist[nextvisdesc].classList.contains("haspdf") == true){ //if there's a pdf
-        reveal(droplist[nextvisdesc].querySelector(".window")); //show pdf not bigimg
-        hide(droplist[nextvisdesc].querySelector(".bigimg"))
-    } 
-    else{
-        bigimgs[0].src = thmnails[0].src; //otherwise expand the first thmnail
-    }
+    hide(projectlist[currentvisproj]);
+    show(projectlist[prevproj]);
+    displayfirstdetail(projectlist[prevproj]);
 }
 
-function nex(){
-    var currentvisdesc = undefined 
-
-    for (j = 0; j <droplist.length; j++) { //checking which desc isvisible
-        
-        if ( isvisible(droplist[j]) == true) {
-            currentvisdesc = j; //storing it
-        }
-    }
-
-    nextvisdesc = currentvisdesc + 1; //setting next desc
-
-    if (nextvisdesc >= droplist.length){ //on the last desc, next desc is the first desc.....
-        nextvisdesc = 0; 
-    }
-
-    hide(droplist[currentvisdesc]);
-    reveal(droplist[nextvisdesc]);
-
-    var bigimgs = droplist[nextvisdesc].getElementsByClassName("bigimg");
-    var thmnails = droplist[nextvisdesc].getElementsByClassName("descimg");
+function nex(){ //goes forward one project
     
-    if (droplist[nextvisdesc].classList.contains("haspdf") == true) //if there's a pdf
-    {
-        reveal(droplist[nextvisdesc].querySelector(".window")); //show pdf not bigimg
-        hide(droplist[nextvisdesc].querySelector(".bigimg"))
-    } 
-    else{
-        bigimgs[0].src = thmnails[0].src; //otherwise expand the first thmnail
+    var currentvisproj;
+
+    for ( i = 0; i <projectlist.length; i++ ) { //checking which project isvisible
+        if ( isvisible(projectlist[i]) ) {
+            currentvisproj = i;
+        }
     }
+
+    nextproj = currentvisproj + 1; //finding next project
+    if ( nextproj >= projectlist.length ){ //looping array or something
+        nextproj = 0; 
+    }
+
+    hide(projectlist[currentvisproj]);
+    show(projectlist[nextproj]);
+    displayfirstdetail(projectlist[nextproj]);
 }
 
 function enlarge(hoveredimg) { //enlarges thumbnails
 
     var newimgsrc = hoveredimg.src;
-    var allbigimgs = document.getElementsByClassName("bigimg");
-    var allpdfwindows = document.getElementsByClassName("window");
+
+    var corrbigimg = hoveredimg.parentElement.parentElement.querySelector(".bigimg");
+    var corrwindow = hoveredimg.parentElement.parentElement.querySelector(".window");
     
-    if (hoveredimg.classList.contains("pdfthm") == true){ //if thumbnail is tagged as pdf
+    if ( hoveredimg.classList.contains("pdfthm") ){ 
+        show(corrwindow);
+        hide(corrbigimg);
+    }else{
+        hide(corrwindow);
+        show(corrbigimg);
+        corrbigimg.src = newimgsrc; //change source to match hovered image
+    }
+}
 
-        for (k=0; k<allpdfwindows.length; k++) { //show all pdf windows
-            reveal(allpdfwindows[k]);
-        }
-        for (j=0; j<allbigimgs.length; j++) { //hide all bigimg
-            hide(allbigimgs[j]);
-        }
+function displayfirstdetail(project){ //displays "window" or "bigimg" based on first detail image
 
-    }else{ //if thumbanil is not tagged as pdf
-        
-        for (h=0; h<allpdfwindows.length; h++) { //hide all pdf windows
-            hide(allpdfwindows[h]);
-        }
-        for (k=0; k<allbigimgs.length; k++) { //show all bigimg
-            reveal(allbigimgs[k]);
-            allbigimgs[k].src = newimgsrc; //choose which bigimg to show
-        }
+    var corrwindow = project.querySelector(".window");
+    var corrbigimg = project.querySelector(".bigimg");
+    var firstdetailimg = project.querySelector(".descimg");
+
+    if ( project.classList.contains("haspdf") ){
+        show(corrwindow);
+        hide(corrbigimg);
+    }else{
+        hide(corrwindow);
+        show(corrbigimg);
+        corrbigimg.src = firstdetailimg.src;
     }
 }
